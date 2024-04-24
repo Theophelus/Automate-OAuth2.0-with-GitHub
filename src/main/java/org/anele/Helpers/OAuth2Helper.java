afb40c6d77da0368ef78d82991d871259266ff98
Theophelus <aneletom10@gmail.com>
2024-04-24 20:07:50 +0200

    adding two method for extracting authorization code and returning it

--- src/main/java/org/anele/Helpers/OAuth2Helper.java
+++ src/main/java/org/anele/Helpers/OAuth2Helper.java
@@ -1,22 +1,73 @@
 package org.anele.Helpers;
-
-import org.anele.base.DriverFactory;
-
 import java.text.MessageFormat;
+
+import static org.anele.base.DriverFactory.*;
+import static org.anele.pages.LoginIntoGitHubPage.*;
+import static org.anele.pages.LoginIntoGitHubPage.getCurrentUrl;
 
 public class OAuth2Helper {
 
 
     //method to build Auth Url with base url, client id and scope as parameters
     public static StringBuilder buildUrl(String baseUrl,
-                                             String clientId,
-                                         String scope){
+                                         String clientId,
+                                         String scope) {
 
-        return new StringBuilder(baseUrl)
-                .append("?")
-                .append(MessageFormat.format("client_id={0}", clientId))
-                .append("&")
-                .append(MessageFormat.format("scope={0}", scope));
+       try {
+
+         StringBuilder builder=  new StringBuilder(baseUrl)
+                   .append("?")
+                   .append(MessageFormat.format("client_id={0}", clientId))
+                   .append("&")
+                   .append(MessageFormat.format("scope={0}", scope));
+
+           System.out.println("Base url: " + builder);
+
+           return builder;
+
+       } catch (Exception e){
+           System.out.println("Base URL not built successfully" + e.getMessage());
+           return null;
+       }
+    }
+
+    public static String returnOAuthorizationCode(String browser,
+            String baseUrl, String clientId, String scope, String username, String password) {
+
+        //get current browser to extract auth code
+        getCurrentBrowser(browser);
+        //build url
+        StringBuilder URL = buildUrl(baseUrl, clientId, scope);
+        //launch the browser
+        assert URL != null;
+        getDriver().get(URL.toString());
+        //login into the login page
+        loginIntoGitHub(username, password);
+        //get currentUrl
+        String getCode = getCurrentUrl();
+        //extract authorization code
+        return extractCode(getCode);
+    }
+
+    //create a method to extract authorization code, after logging in into gitHub application
+    private static String extractCode(String code) {
+
+        String authorizationCode = null;
+        try {
+
+            if (code.contains("=")) {
+                //split the string based on the delimiter and get the entry at index 1
+                authorizationCode = code.split("=")[1];
+
+            }
+            //return the auth code
+            return authorizationCode;
+
+        } catch (Exception e) {
+            System.out.println("Error occurred while trying to extract authorization code:" + e.getMessage());
+            return null;
+        }
+
     }
 
 }

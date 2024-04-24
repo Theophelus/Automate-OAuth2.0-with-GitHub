894a0c10e3ecb999e791ef7a4580af26c46f235c
Theophelus <aneletom10@gmail.com>
2024-04-24 14:24:40 +0200

    Adding a DriverFactory.java that is thread safe

---
+++ src/main/java/org/anele/base/DriverFactory.java
@@ -0,0 +1,66 @@
+package org.anele.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ThreadGuard;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DriverFactory {
+    protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    //get The driver from DriverPage
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    //add a method to getCurrentBrowser with one parameter
+    public static void getCurrentBrowser(String browser) {
+        initializeDrivers(browser);
+    }
+
+    //Define a method to initializeDrivers
+    static void initializeDrivers(String browser) {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        EdgeOptions edgeOptions = getEdgeOptions(capabilities);
+
+        if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            //make sure WebDriver is only called by the thread that created it using ThreadGuard.protect()
+            driver.set(ThreadGuard.protect(new EdgeDriver(edgeOptions)));
+        }
+        maximizeBrowser(getDriver());
+    }
+
+    //getChromeOptions
+    static EdgeOptions getEdgeOptions(DesiredCapabilities capabilities) {
+        EdgeOptions options = new EdgeOptions();
+        List<String> argumentList = new ArrayList<>();
+        argumentList.add("--disable-gbu");
+        argumentList.add("--no-sandbox");
+        argumentList.add("--headless=new");
+        options.addArguments(argumentList);
+        capabilities.setCapability(EdgeOptions.CAPABILITY, options);
+        options.merge(capabilities);
+        return options;
+    }
+
+    static void maximizeBrowser(WebDriver driver) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+    }
+
+    //quit drivers
+    public static void quitDrivers() {
+        getDriver().quit();
+        driver.remove();
+    }
+
+
+}

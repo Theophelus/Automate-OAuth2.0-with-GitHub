998e021031524c5de8e839aae470d50a002faf39
Theophelus <aneletom10@gmail.com>
2024-04-24 14:32:29 +0200

    Creating a class to read config.properties file

---
+++ src/main/java/org/anele/utils/ReadConfigFile.java
@@ -0,0 +1,10 @@
+package org.anele.utils;
+
+import java.util.Properties;
+
+public class ReadConfigFile {
+
+    private static Properties properties = null;
+    private static final String ConfigFilePath = "src/main/resources/config.properties";
+
+}

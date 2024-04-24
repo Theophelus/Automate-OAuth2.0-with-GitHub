bea8261c19a371adb5d0608e8a4ac65e9a2c0b59
Theophelus <aneletom10@gmail.com>
2024-04-24 18:31:18 +0200

    Adding a class to read config.properties file and its attributes

---
+++ src/main/java/org/anele/utils/ReadConfigFileUtil.java
@@ -0,0 +1,48 @@
+package org.anele.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadConfigFileUtil {
+
+    private static Properties properties = null;
+
+    public ReadConfigFileUtil() {
+        properties = new Properties();
+        loadProperties();
+    }
+
+    private synchronized void loadProperties() {
+
+        try (FileInputStream file = new FileInputStream("src/main/resources/config.properties")) {
+            //load the file
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while trying to load the config file: " + e);
+        }
+    }
+
+    public static String getBrowser() {
+        return properties.getProperty("browser");
+    }
+
+    public static String getBaseURL() {
+        return properties.getProperty("baseURL");
+    }
+
+    public static String getClientId() {
+        return properties.getProperty("clientId");
+    }
+
+    public static String getScope() {
+        return properties.getProperty("scope");
+    }
+    public static String getUsername(){
+        return properties.getProperty("username");
+    }
+
+    public static String getPassword(){
+        return properties.getProperty("password");
+    }
+}

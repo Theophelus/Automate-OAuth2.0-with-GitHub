4182b78604995cee520825bf9d83bdb9df05e701
Theophelus <aneletom10@gmail.com>
2024-04-25 10:59:15 +0200

    adding a utils class to be able to write into the config.properties file to store and keep track of access token

---
+++ src/main/java/org/anele/utils/WriteToConfigFileUtils.java
@@ -0,0 +1,18 @@
+package org.anele.utils;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class WriteToConfigFileUtils {
+    //define properties file
+    private static Properties properties = null;
+
+    public WriteToConfigFileUtils() {
+        properties = new Properties();
+    }
+
+
+
+}

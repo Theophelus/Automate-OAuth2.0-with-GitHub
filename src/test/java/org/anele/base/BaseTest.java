6aabb56d5e361030ac0a387fcd9f0eadbc5f5a26
Theophelus <aneletom10@gmail.com>
2024-04-27 15:21:26 +0200

    Adding a getRequestSpec for reusable request and getting access token

--- src/test/java/org/anele/base/BaseTest.java
+++ src/test/java/org/anele/base/BaseTest.java
@@ -1,11 +1,22 @@
 package org.anele.base;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.anele.Helpers.OAuth2Helper;
 import org.anele.utils.ReadConfigFileUtil;
 import org.anele.utils.WriteToConfigFileUtils;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 import static org.anele.utils.ReadConfigFileUtil.*;
+import static org.anele.utils.WriteToConfigFileUtils.setOAuthToken;
 
 public class BaseTest extends OAuth2Helper {
     protected static String code = "";
@@ -39,4 +50,12 @@ public class BaseTest extends OAuth2Helper {
 
     }
 
+    //define a method, to get requestSpecification builder for oauth end point
+    private static RequestSpecification getRequestSpec(String oauthBaseUrl) {
+
+        return new RequestSpecBuilder()
+                .setAccept(ContentType.JSON)
+                .setBaseUri(oauthBaseUrl)
+                .build();
+    }
 }

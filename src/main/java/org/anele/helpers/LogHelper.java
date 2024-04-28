3daf20353800abe0d0131bdc92eb789940c6fbd8
Theophelus <aneletom10@gmail.com>
2024-04-28 13:23:22 +0200

    Adding a  LogHelper.java class for code reuse

---
+++ src/main/java/org/anele/helpers/LogHelper.java
@@ -0,0 +1,28 @@
+package org.anele.helpers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogHelper {
+
+    //define a Logger object
+    public static Logger log;
+
+    //create a generic Logger constructor for class tracking
+    public LogHelper(Class<?> _class) {
+        log = LogManager.getLogger(_class);
+    }
+
+    //log level methods
+    public static void info(String message) {
+        log.info(message);
+    }
+
+    public static void error(String message, String e) {
+        log.error(message, e);
+    }
+
+    public static void endTest() {
+        log.info("************************* " + "E*N*D" + " ***********************");
+    }
+}

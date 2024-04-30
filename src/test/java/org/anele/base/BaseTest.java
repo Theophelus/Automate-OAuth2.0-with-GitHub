4ed0053bcc5a621da3624600e144007ccd7549a3
Theophelus <aneletom10@gmail.com>
2024-04-30 19:05:26 +0200

    Refactoring BaseTest.java and generating a new token before each suite run, to improve performance, then build Https Request in the BeforeSuite

--- src/test/java/org/anele/base/BaseTest.java
+++ src/test/java/org/anele/base/BaseTest.java
@@ -1,4 +1,6 @@
 package org.anele.base;
+
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -8,10 +10,9 @@ import org.anele.helpers.LogHelper;
 import org.anele.utils.ReadConfigFileUtil;
 import org.anele.utils.WriteToConfigFileUtils;
 import org.apache.http.HttpStatus;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,19 +23,32 @@ import static org.anele.utils.WriteToConfigFileUtils.setOAuthToken;
 public class BaseTest extends OAuth2Helper {
     static LogHelper log = new LogHelper(BaseTest.class);
     protected static String code = "";
+    //define Request specification to build a request with access token;
+    RequestSpecification httpRequest;
 
     public BaseTest() {
         new ReadConfigFileUtil();
         new WriteToConfigFileUtils();
     }
 
-    @BeforeClass
-    void setup() {
+    @BeforeSuite
+    public void setup() throws Exception {
         generateSessionCode();
-        access_token();
+        //build a request
+        httpRequest = new RequestSpecBuilder()
+                .setBaseUri(ReadConfigFileUtil.getBaseUrl())
+                .setAccept(ContentType.JSON)
+                .addHeader("Authorization", "Bearer " + access_token())
+                .build();
+
     }
 
-    @AfterClass
+    @Test
+    void sample() {
+        System.out.println("Welcome home..." + httpRequest);
+    }
+
+    @AfterSuite
     void tearDown() {
         DriverFactory.quitDrivers();
     }
@@ -44,7 +58,7 @@ public class BaseTest extends OAuth2Helper {
         code = OAuth2Helper.returnOAuthorizationCode
                 (
                         getBrowser(),
-                        getBaseURL(),
+                        getOAuthBaseURL(),
                         getClientId(), getScope(),
                         getUsername(),
                         getPassword()
@@ -61,7 +75,7 @@ public class BaseTest extends OAuth2Helper {
     }
 
     //extract access token
-    protected static String access_token() {
+    public static String access_token() throws Exception {
 
         RequestSpecification httpRequest = getRequestSpec(getOAuthTokenURL());
         Map<String, String> param = new HashMap<>();
@@ -72,28 +86,29 @@ public class BaseTest extends OAuth2Helper {
 
         try {
             //build the response
-
             Response httpResponse = given()
                     .spec(httpRequest)
                     .formParams(param)
                     .post();
 
             if (httpResponse.statusCode() != HttpStatus.SC_OK) {
-//                logHelper.error();
-                return null;
+                throw new Exception("Failed to get access token from the request " + httpResponse.statusCode());
             }
 
-            //save into config.properties file
-            setOAuthToken(httpResponse.jsonPath().getString("access_token"));
-            //return token
-            log.info("Access token extracted successfully: "
-                    + httpResponse.jsonPath().getString("access_token"));
-            return httpResponse.jsonPath().getString("access_token");
+            String accessToken = httpResponse.jsonPath().get("access_token");
 
-        } catch (Exception e) {
+            log.info("Access token extracted successfully: "
+                    + accessToken);
+            //save into config.properties file
+            setOAuthToken(accessToken);
+            //return token
+            return accessToken;
+
+        } catch (IOException e) {
             log.error("Error occurred while trying to extract access token: ",
                     e.getMessage());
-            return null;
+            throw new Exception("Error occurred while trying to extract access token:"
+                    + e.getMessage(), e);
         }
     }
 }

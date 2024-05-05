d94140b13b6ecfdd89a6b3dfacda7444ce5dbea0
Theophelus <aneletom10@gmail.com>
2024-05-05 16:28:49 +0200

    Adding a UserTests.java

---
+++ src/test/java/org/anele/tests/UserTests.java
@@ -0,0 +1,15 @@
+package org.anele.tests;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import org.anele.calls.UserApiCalls;
+import org.anele.pojo.User;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class UserTests extends UserApiCalls {
+
+
+}

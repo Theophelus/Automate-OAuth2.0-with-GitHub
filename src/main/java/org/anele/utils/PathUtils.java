e57f19c28669df890fb4822f7e25460fe19645bc
Theophelus <aneletom10@gmail.com>
2024-06-23 17:11:14 +0200

    Adding more endpoints on PathUtils.java enum, for possibly expanding tests

--- src/main/java/org/anele/utils/PathUtils.java
+++ src/main/java/org/anele/utils/PathUtils.java
@@ -1,11 +1,16 @@
 package org.anele.utils;
 
 public enum PathUtils {
-    GET_SPECIFIC_USER("/users/{username}");
+    GET_SPECIFIC_USER("/users/{username}"),
+    GET_A_REPOSITORIES("/repos/{owner}/{repo}"),
+    PATCH_UPDATE_A_REPOSITORY("/repos/{owner}/{repo}"),
+    REPOSITORY_CONTRIBUTORS("/repos/{owner}/{repo}/contributors");
     private final String path;
+
     PathUtils(String path) {
         this.path = path;
     }
+
     public String getPath() {
         return path;
     }

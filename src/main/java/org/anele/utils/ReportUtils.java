a0de9bdd1787549b4d63367a1e5585a48eea5407
Theophelus <aneletom10@gmail.com>
2024-06-24 17:06:12 +0200

    Adding a ReportUtils.java class that implements IReporter interface and override generateReport method

---
+++ src/main/java/org/anele/utils/ReportUtils.java
@@ -0,0 +1,13 @@
+package org.anele.utils;
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
+import java.util.*;
+
+public class ReportUtils implements IReporter {
+    @Override
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+
+    }
+
+}

cd4880f8731daaf8eb6b95aa1d029b7c95e3db65
Theophelus <aneletom10@gmail.com>
2024-07-15 18:25:27 +0200

    Adding a new class to decide based on environment execution

---
+++ src/main/java/org/anele/utils/ConfigFileUtils.java
@@ -0,0 +1,51 @@
+package org.anele.utils;
+
+public class ConfigFileUtils {
+
+    static LocalConfigUtils local = null;
+    static CiConfigFileUtils ci = null;
+    static boolean platform = false;
+
+    //initial different environments
+    static {
+        if (platform) {
+            ci = new CiConfigFileUtils();
+        } else
+            local = new LocalConfigUtils();
+    }
+    public static String getBrowser() {
+        return platform ? ci.getBrowser() : local.getBrowser();
+    }
+
+    public static String getOAuthBaseURL() {
+        return platform ? ci.getOAuthBaseURL() : local.getOAuthBaseURL();
+    }
+
+    public static String getClientId() {
+        return platform ? ci.getClientId() : local.getClientId();
+    }
+
+    public static String getOAuthTokenURL() {
+        return platform ? ci.getOAuthTokenURL() : local.getOAuthTokenURL();
+    }
+
+    public static String getClientSecret() {
+        return platform ? ci.getClientSecret() : local.getClientSecret();
+    }
+
+    public static String getScope() {
+        return platform ? ci.getScope() : local.getScope();
+    }
+
+    public static String getUsername() {
+        return platform ? ci.getUsername() : local.getUsername();
+    }
+
+    public static String getPassword() {
+        return platform ? ci.getPassword() : local.getPassword();
+    }
+
+    public static String getBaseUrl() {
+        return platform ? ci.getBaseUrl() : local.getBaseUrl();
+    }
+}
